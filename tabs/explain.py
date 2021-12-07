219b9d310a0175a85a5399b1606ca04c48e9cc3e
KitC3 <56023633+KitC3@users.noreply.github.com>
2021-12-07 13:14:21 +0100

    Update explain.py

--- tabs/explain.py
+++ tabs/explain.py
@@ -7,7 +7,7 @@ from app import app
 layout = [dcc.Markdown("""
 #### The Project Goal and Data
 
-The goal of the project is to predict the decommission time of a building in the Netherlands by using open source data. This end product would then be presented as a minimum value product (MVP)
+The goal of the project is to predict the decommission time of a building in the Netherlands by using open source data. This end product would then be presented as a minimum valuable product (MVP)
 for the company New Horizon. 
 
 #### The Data and Evaluation Protocol
