c0dca05207227141f67ce8b372af38059e0ed50c
KitC3 <56023633+KitC3@users.noreply.github.com>
2021-12-07 13:15:40 +0100

    Update predict.py

--- tabs/predict.py
+++ tabs/predict.py
@@ -36,9 +36,9 @@ layout = html.Div([
     ], style=style),
     
     html.Div([
-    dcc.Markdown('###### Decommision Time'),
+    dcc.Markdown('###### Decommission Time'),
     dcc.Input(
-            id='decommision_time',
+            id='decommission_time',
             placeholder="",
             value=11
         ),
@@ -51,8 +51,8 @@ html.Img(src='https://raw.githubusercontent.com/KitC3/DEiA1/main/dashboard_rest.
 @app.callback(
     Output('prediction-content', 'children'),
     [Input('data_input', 'value'),
-     Input('decommision_time', 'value')])
-def predict(data_input, decommision_time):
+     Input('decommission_time', 'value')])
+def predict(data_input, decommission_time):
     col = """bouwjaar,WoningType,NR_CEL,oppervlakte,heeft_bijeenkomst_functie,heeft_cel_functie,heeft_gezondheids_functie,heeft_industrie_functie,heeft_kantoor_functie,heeft_logies_functie,heeft_onderwijs_functie,heeft_overig_functie,heeft_sport_functie,heeft_winkel_functie,heeft_woon_functie,X,Y,post_code,woonplaats,baro,INWONER,INW_014,INW_1524,INW_2544,INW_65PL,GEBOORTE,P_NL_ACHTG,P_WE_MIG_A,P_NW_MIG_A,TOTHH_EENP,TOTHH_MPZK,HH_EENOUD,HH_TWEEOUD,GEM_HH_GR,WONING,WONVOOR45,WON_4564,WON_6574,WON_7584,WON_8594,WON_9504,WON_0514,WON_1524,WON_MRGEZ,P_KOOPWON,P_HUURWON,WON_HCORP,WON_NBEW,WOZWONING,G_GAS_WON,G_ELEK_WON,M_INKHH,P_LINK_HH,UITKMINAOW,OAD,STED,straat_naam_nr"""
     col_names = re.sub("[^\w]", " ",  col).split()
     df = pd.read_csv(io.StringIO(data_input), sep=",", header=None, names = col_names)
@@ -61,12 +61,12 @@ def predict(data_input, decommision_time):
     y_pred_log = pipeline.predict(df)
     y_pred = y_pred_log[0]
     
-    if float(decommision_time) > 0:
-        difference = ((y_pred - float(decommision_time)) / float(decommision_time)) * 100
+    if float(decommission_time) > 0:
+        difference = ((y_pred - float(decommission_time)) / float(decommission_time)) * 100
         if difference < 0:
-            results = f'The predicted decommission time is {y_pred:.2f} years which is {difference:.2f}% under the actual decommission time ({decommision_time} years).'
+            results = f'The predicted decommission time is {y_pred:.2f} years which is {difference:.2f}% under the actual decommission time ({decommission_time} years).'
         else:
-            results = f'The predicted decommission time is {y_pred:.2f} years which is {difference:.2f}% over the actual decommission time ({decommision_time} years).'
+            results = f'The predicted decommission time is {y_pred:.2f} years which is {difference:.2f}% over the actual decommission time ({decommission_time} years).'
     else:
         results = f'The predicted decommission time is {y_pred:.2f} years.'
 
